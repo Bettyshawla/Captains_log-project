68f4c360d8654c8770d7385139649316a77bdb43
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2022-11-28 17:48:56 -0500

    Create

--- server.js
+++ server.js
@@ -1,27 +1,53 @@
 const express = require("express");
 const app = express();
 const PORT = 3000;
-const reactView = require("express-react-views")
+const reactView = require("express-react-views");
+const bodyParser = require("body-parser")
 
 
 app.set("view engine", "jsx")
 app.engine("jsx", reactView.createEngine())
 
 
+// body-parser
+// these two middle ware will help us get the data from the input field
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// app.use(bodyParser.urlencoded({
+//     extended: true
+//   }));
+
+// parse app/json
+app.use(bodyParser.json())
+
+
 app.use((req, res, next) => {
     console.log("Im running for all routes")
     console.log("1. middleware")
     next()
-  })
-
-app.get('/', (req, res)=>{
-    res.send("<h1>Welcome to the Captains Log App!</h1>, <h2> second </h2>")
 })
 
-app.get('/new', (req, res) => {
+app.get('/', (req, res) => {
+    res.send("<h1>Welcome to the Captains Log App!</h1>")
+})
+
+app.get('/logs/new', (req, res) => {
     console.log("2. controller")
     res.render('New')
 })
+
+app.post("/logs/", (req, res) => {
+
+    if (req.body.shipIsBroken === "on") {
+        req.body.shipIsBroken = true;
+    } else {
+        req.body.shipIsBroken = false;
+    }
+
+    res.send(req.body)
+
+
+});
 
 app.listen(PORT, () => {
     console.log("Listening in port 3000")
--- views/New.jsx
+++ views/New.jsx
@@ -4,19 +4,22 @@ class New extends React.Component {
     render() {
         return (
             <div>
-                <h1> This is the new Captain Log app</h1>
-                <form action="/logs" method="POST">
-                    Title: <input type="text" />
-                    <br />
-                    <br />
-                    Entry: <input type="text" />
-                    <br />
-                    <br />
-                    Ship Is Broken <input type="checkbox" />
-                    <br />
-                    <br />
-                    <input type="Submit" />
-                </form>
+                <body>
+
+                    <h1> This is the new Captain Log app</h1>
+                    <form action="/logs/" method="POST">
+                        Title: <input type="text" name="title" />
+                        <br />
+                        <br />
+                        Entry: <input type="textentry" name="entry" />
+                        <br />
+                        <br />
+                        Ship Is Broken: <input type="checkbox" name="ship is broken"/>
+                        <br />
+                        <br />
+                        <input type="Submit" value="submit"/>
+                    </form>
+                </body>
             </div>
         )
     }
