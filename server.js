d690b0a951587d2aed461ad61f5440ae958cbc2c
BettyShawlagit config --global user.email Betty.tada1@gmail.com <betty.tada1@gmail.com>
2022-11-29 15:30:00 -0500

    Upgraded my Create Route

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const bodyParser = require("body-parser");
 // const Logs = require("./models/logs")
 const mongoose = require("mongoose")
 
-//  Mongoose
+// ============= Mongoose =============
 mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -16,16 +16,16 @@ mongoose.connect(process.env.MONGO_URI, {
     console.log("connected to mongo")
   })
 
+// =============  Engine =============
 app.set("view engine", "jsx")
 app.engine("jsx", reactView.createEngine())
 
-
+// ============= Middle Ware =============
 // body-parser
 // these two middle ware will help us get the data from the input field
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse app/json
 app.use(bodyParser.json())
-
 
 app.use((req, res, next) => {
     console.log("Im running for all routes")
@@ -33,10 +33,13 @@ app.use((req, res, next) => {
     next()
 })
 
+
+// ============= Routes =============
 app.get('/', (req, res) => {
     res.send("<h1>Welcome to the Captains Log App!</h1>")
 })
 
+// ============= New Route =============
 app.get('/logs/new', (req, res) => {
     console.log("2. controller")
     res.render('New')
@@ -48,9 +51,13 @@ app.post("/logs/", (req, res) => {
     } else {
         req.body.shipIsBroken = false;
     }
-    res.send(req.body)
+    // res.send(req.body)
+    res.redirect("/logs/ShowPage") //Come back to it after Show is created
+
 });
 
+
+// ============= Listening PORT =============
 app.listen(PORT, () => {
-    console.log("Listening in port 3000")
+    console.log("Listening in port: 3000")
 })
